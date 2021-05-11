cd0879c4ed8e7e1209f3d1e81cad47fcc9919c84
smail04 <alex8miros@gmail.com>
2021-05-11 15:51:29 +0300

    add health

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -6,11 +6,26 @@ using UnityEngine.AI;
 public class Unit : SelectableObject
 {
     public int price;
+    public int health;
     public NavMeshAgent navMeshAgent;
 
     public override void WhenClickOnGround(Vector3 point)
     {
         base.WhenClickOnGround(point);
         navMeshAgent.SetDestination(point);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
     }
 }
